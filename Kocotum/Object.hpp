49a26c71387fd588a29a47c0afc79f65f7740284
Tanorenn <kuronosu102938@gmail.com>
2024-06-27 22:41:53 +0900

    オブジェクトの基底クラスとなるObjectクラスを追加

---
+++ Kocotum/Object.hpp
@@ -0,0 +1,60 @@
+﻿# pragma once
+# include <Siv3D.hpp>
+
+
+const Vec2 CHIP_SIZE = Vec2{ 50, 50 };
+
+enum class E_Object
+{
+	Wall,
+	Needle,
+	MiniNeedle,
+	GravityLine,
+	JumpToggleWall,
+	JumpToggleNeedle,
+	MoveFloorHorizontal,
+	MoveFloorVertical,
+};
+
+enum class E_Direction : uint16
+{
+	Up,
+	Right,
+	Down,
+	Left,
+};
+
+struct Player;
+
+/// @brief マップ上に配置されているオブジェクトの基底クラス
+struct Object
+{
+	Vec2 pos;
+	Effect& effect;
+	Player& player;
+	E_Object type;
+
+	Object(Vec2 pos, Effect& effect, Player& player, E_Object type)
+		: pos{ pos }
+		, effect{ effect }
+		, player{ player }
+		, type{ type }
+	{ }
+
+	virtual void restart() = 0;
+
+	virtual bool intersectsPlayer() = 0;
+
+	virtual bool mouseOver() = 0;
+
+	virtual void handleCollisionX() = 0;
+
+	virtual void handleCollisionY() = 0;
+
+	virtual void update() = 0;
+
+	virtual void draw() const = 0;
+
+private:
+
+};
