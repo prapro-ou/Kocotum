46be5f23c84a9d3b2f480e57b267c9d6f4bceebf
Tanorenn <kuronosu102938@gmail.com>
2024-07-05 21:38:59 +0900

    Needleを追加

---
+++ Kocotum/Needle.cpp
@@ -0,0 +1,252 @@
+﻿#include "Needle.hpp"
+
+Needle::Needle(Vec2 pos, Effect& effect, Player& player, E_Direction direction)
+	: Object{ pos, effect, player, E_ObjectType::Needle, U"針" }
+	, direction{ direction }
+	, mouseOverBody{ RectF{ pos, CHIP_SIZE } }
+	, body{ Triangle{ Vec2{ 0, CHIP_SIZE.y }, Vec2{ CHIP_SIZE.x / 2, 0 }, Vec2{ CHIP_SIZE } } }
+{
+	body.moveBy(pos);
+	body = body.rotatedAt(pos + CHIP_SIZE / 2, (uint8)direction * 90_deg);
+}
+
+void Needle::setDirection(E_Direction direction)
+{
+	// 回転角度を計算し、針の向きと形状を更新
+	int8 rotate = (uint8)direction - (uint8)this->direction;
+	this->direction = direction;
+	body = body.rotatedAt(pos + CHIP_SIZE / 2, rotate * 90_deg);
+}
+
+void Needle::restart()
+{
+	// 再起動時の処理（現在は何もしない）
+}
+
+bool Needle::intersectsPlayer()
+{
+	return body.intersects(player.body);
+}
+
+bool Needle::mouseOver()
+{
+	return mouseOverBody.mouseOver();
+}
+
+void Needle::setPos(Vec2 pos)
+{
+	mouseOverBody.setPos(pos);
+	body.moveBy(this->pos - pos);
+	this->pos = pos;
+}
+
+void Needle::handleCollisionX()
+{
+	if (player.isAlive)
+	{
+		// プレイヤーが生きている場合、死亡エフェクトを追加し、プレイヤーを死亡状態にする
+		effect.add<DeathEffect>(player.body.center());
+		player.isAlive = false;
+	}
+}
+
+void Needle::handleCollisionY()
+{
+	if (player.isAlive)
+	{
+		// プレイヤーが生きている場合、死亡エフェクトを追加し、プレイヤーを死亡状態にする
+		effect.add<DeathEffect>(player.body.center());
+		player.isAlive = false;
+	}
+}
+
+void Needle::update()
+{
+	// 更新処理（現在は何もしない）
+}
+
+void Needle::draw() const
+{
+	// 針のテクスチャを描画
+	TextureAsset(U"Needle").resized(CHIP_SIZE).rotated((uint8)direction * 90_deg).draw(pos);
+}
+
+
+MiniNeedle::MiniNeedle(Vec2 pos, Effect& effect, Player& player, E_Direction direction)
+	: Object{ pos, effect, player, E_ObjectType::MiniNeedle, U"小さい針" }
+	, mouseOverBody{ RectF{ pos, CHIP_SIZE } }
+	, direction{ direction }
+{
+	// 4つの小さな三角形を作成
+	body << Triangle{ Vec2{ 0, CHIP_SIZE.y }, Vec2{ CHIP_SIZE.x / 8.0, CHIP_SIZE.y * 3.0 / 4.0 }, Vec2{ CHIP_SIZE.x / 4.0, CHIP_SIZE.y } };
+	body << Triangle{ Vec2{ CHIP_SIZE.x / 4.0, CHIP_SIZE.y }, Vec2{ CHIP_SIZE.x * 3.0 / 8.0, CHIP_SIZE.y * 3.0 / 4.0 }, Vec2{ CHIP_SIZE.x / 2.0, CHIP_SIZE.y } };
+	body << Triangle{ Vec2{ CHIP_SIZE.x / 2.0, CHIP_SIZE.y }, Vec2{ CHIP_SIZE.x * 5.0 / 8.0, CHIP_SIZE.y * 3.0 / 4.0 }, Vec2{ CHIP_SIZE.x * 3 / 4.0, CHIP_SIZE.y } };
+	body << Triangle{ Vec2{ CHIP_SIZE.x * 3.0 / 4.0, CHIP_SIZE.y }, Vec2{ CHIP_SIZE.x * 7.0 / 8.0, CHIP_SIZE.y * 3.0 / 4.0 }, Vec2{ CHIP_SIZE } };
+
+	for (auto& b : body)
+	{
+		b.moveBy(pos);
+		b = b.rotatedAt(pos + CHIP_SIZE / 2.0, (uint16)direction * 90_deg);
+	}
+}
+
+void MiniNeedle::setDirection(E_Direction direction)
+{
+	// 回転角度を計算し、各三角形の向きを更新
+	int8 rotate = (uint8)direction - (uint8)this->direction;
+	this->direction = direction;
+	for (auto& b : body)
+	{
+		b = b.rotatedAt(pos + CHIP_SIZE / 2.0, rotate * 90_deg);
+	}
+}
+
+void MiniNeedle::restart()
+{
+	// 再起動時の処理（現在は何もしない）
+}
+
+bool MiniNeedle::intersectsPlayer()
+{
+	// いずれかの三角形とプレイヤーが交差しているか確認
+	for (auto& b : body)
+	{
+		if (b.intersects(player.body))
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+bool MiniNeedle::mouseOver()
+{
+	return mouseOverBody.mouseOver();
+}
+
+void MiniNeedle::setPos(Vec2 pos)
+{
+	mouseOverBody.setPos(pos);
+	for (auto& b : body)
+	{
+		b.moveBy(pos - this->pos);
+	}
+	this->pos = pos;
+}
+
+void MiniNeedle::handleCollisionX()
+{
+	if (player.isAlive)
+	{
+		// プレイヤーが生きている場合、死亡エフェクトを追加し、プレイヤーを死亡状態にする
+		effect.add<DeathEffect>(player.body.center());
+		player.isAlive = false;
+	}
+}
+
+void MiniNeedle::handleCollisionY()
+{
+	if (player.isAlive)
+	{
+		// プレイヤーが生きている場合、死亡エフェクトを追加し、プレイヤーを死亡状態にする
+		effect.add<DeathEffect>(player.body.center());
+		player.isAlive = false;
+	}
+}
+
+void MiniNeedle::update()
+{
+	// 更新処理（現在は何もしない）
+}
+
+void MiniNeedle::draw() const
+{
+	// 各三角形を描画
+	for (const auto& b : body)
+	{
+		b.draw().drawFrame(2, 0, Palette::Dimgrey);
+	}
+}
+
+JumpToggleNeedle::JumpToggleNeedle(Vec2 pos, Effect& effect, Player& player, bool init, E_Direction direction)
+	: Object{ pos, effect, player, E_ObjectType::JumpToggleNeedle, U"ジャンプで切り替わる針" }
+	, direction{ direction }
+	, mouseOverBody{ RectF{ pos, CHIP_SIZE } }
+	, body{ Triangle{ Vec2{ 0, CHIP_SIZE.y }, Vec2{ CHIP_SIZE.x / 2, 0 }, Vec2{ CHIP_SIZE } } }
+	, init{ init }
+	, isOn{ init }
+{
+	body.moveBy(pos);
+	body = body.rotatedAt(pos + CHIP_SIZE / 2, (uint8)direction * 90_deg);
+}
+
+void JumpToggleNeedle::setDirection(E_Direction direction)
+{
+	// 回転角度を計算し、針の向きと形状を更新
+	int8 rotate = (uint8)direction - (uint8)this->direction;
+	this->direction = direction;
+	body = body.rotatedAt(pos + CHIP_SIZE / 2, rotate * 90_deg);
+}
+
+void JumpToggleNeedle::restart()
+{
+	isOn = init;
+}
+
+bool JumpToggleNeedle::intersectsPlayer()
+{
+	return body.intersects(player.body);
+}
+
+bool JumpToggleNeedle::mouseOver()
+{
+	return mouseOverBody.mouseOver();
+}
+
+void JumpToggleNeedle::setPos(Vec2 pos)
+{
+	mouseOverBody.setPos(pos);
+	body.moveBy(this->pos - pos);
+	this->pos = pos;
+}
+
+void JumpToggleNeedle::setInit(bool init)
+{
+	this->init = init;
+	this->isOn = init;
+}
+
+void JumpToggleNeedle::handleCollisionX()
+{
+	if (player.isAlive && isOn)
+	{
+		// プレイヤーが生きていて、針がオンの場合、死亡エフェクトを追加し、プレイヤーを死亡状態にする
+		effect.add<DeathEffect>(player.body.center());
+		player.isAlive = false;
+	}
+}
+
+void JumpToggleNeedle::handleCollisionY()
+{
+	if (player.isAlive && isOn)
+	{
+		// プレイヤーが生きていて、針がオンの場合、死亡エフェクトを追加し、プレイヤーを死亡状態にする
+		effect.add<DeathEffect>(player.body.center());
+		player.isAlive = false;
+	}
+}
+
+void JumpToggleNeedle::update()
+{
+	// プレイヤーがジャンプした時に針の状態を切り替える
+	if (player.jumpNum < player.maxJumpNum && KeySpace.down() && player.isAlive)
+	{
+		isOn = !isOn; // 状態を反転
+	}
+}
+
+void JumpToggleNeedle::draw() const
+{
+	// 針の状態に応じたテクスチャを描画
+	TextureAsset((isOn ? U"JumpToggleNeedle" : U"JumpToggleNeedleAlpha")).resized(CHIP_SIZE).rotated((uint8)direction * 90_deg).draw(pos);
+}
---
+++ Kocotum/Needle.hpp
@@ -0,0 +1,127 @@
+﻿#pragma once
+#include <Siv3D.hpp>
+#include "Object.hpp"
+#include "Player.hpp"
+#include "Effect.hpp"
+
+/**
+ * @brief 針オブジェクトを表すクラス
+ */
+struct Needle : public Object
+{
+	/// @brief 針の向き
+	E_Direction direction;
+	/// @brief マウス判定用の矩形
+	RectF mouseOverBody;
+	/// @brief 針の当たり判定用の三角形
+	Triangle body;
+
+	/**
+	 * @brief Needleクラスのコンストラクタ
+	 * @param pos 針の位置
+	 * @param effect エフェクト参照
+	 * @param player プレイヤー参照
+	 * @param direction 針の向き（デフォルトは上向き）
+	 */
+	Needle(Vec2 pos, Effect& effect, Player& player, E_Direction direction = E_Direction::Up);
+
+	/**
+	 * @brief 針の向きを設定する
+	 * @param direction 設定する向き
+	 */
+	void setDirection(E_Direction direction);
+
+	void restart() override;
+	bool intersectsPlayer() override;
+	bool mouseOver() override;
+	void setPos(Vec2 pos) override;
+	void handleCollisionX() override;
+	void handleCollisionY() override;
+	void update() override;
+	void draw() const override;
+};
+
+/**
+ * @brief 小さい針オブジェクトを表すクラス
+ */
+struct MiniNeedle : public Object
+{
+	/// @brief 針の向き
+	E_Direction direction;
+	/// @brief マウス判定用の矩形
+	RectF mouseOverBody;
+	/// @brief 針の当たり判定用の三角形
+	Array<Triangle> body;
+
+	/**
+	 * @brief MiniNeedleクラスのコンストラクタ
+	 * @param pos 針の位置
+	 * @param effect エフェクト参照
+	 * @param player プレイヤー参照
+	 * @param direction 針の向き（デフォルトは上向き）
+	 */
+	MiniNeedle(Vec2 pos, Effect& effect, Player& player, E_Direction direction = E_Direction::Up);
+
+	/**
+	 * @brief 針の向きを設定する
+	 * @param direction 設定する向き
+	 */
+	void setDirection(E_Direction direction);
+
+	void restart() override;
+	bool intersectsPlayer() override;
+	bool mouseOver() override;
+	void setPos(Vec2 pos) override;
+	void handleCollisionX() override;
+	void handleCollisionY() override;
+	void update() override;
+	void draw() const override;
+};
+
+/**
+ * @brief ジャンプで切り替わる針オブジェクトを表すクラス
+ */
+struct JumpToggleNeedle : public Object
+{
+	/// @brief 針の向き
+	E_Direction direction;
+	/// @brief マウス判定用の矩形
+	RectF mouseOverBody;
+	/// @brief 針の当たり判定用の三角形
+	Triangle body;
+	/// @brief 出現の初期状態
+	bool init;
+	/// @brief 現在の状態
+	bool isOn;
+
+	/**
+	 * @brief JumpToggleNeedleクラスのコンストラクタ
+	 * @param pos 針の位置
+	 * @param effect エフェクト参照
+	 * @param player プレイヤー参照
+	 * @param init 初期状態
+	 * @param direction 針の向き（デフォルトは上向き）
+	 */
+	JumpToggleNeedle(Vec2 pos, Effect& effect, Player& player, bool init = true, E_Direction direction = E_Direction::Up);
+
+	/**
+	 * @brief 針の向きを設定する
+	 * @param direction 設定する向き
+	 */
+	void setDirection(E_Direction direction);
+
+	/**
+	 * @brief 初期状態を設定する
+	 * @param init 設定する初期状態
+	 */
+	void setInit(bool init);
+
+	void restart() override;
+	bool intersectsPlayer() override;
+	bool mouseOver() override;
+	void setPos(Vec2 pos) override;
+	void handleCollisionX() override;
+	void handleCollisionY() override;
+	void update() override;
+	void draw() const override;
+};
