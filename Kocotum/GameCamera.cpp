d490b4ec34fab91bdd660b7537aabe9dc2000355
Tanorenn <kuronosu102938@gmail.com>
2024-07-06 22:26:19 +0900

    GameCameraを追加

---
+++ Kocotum/GameCamera.cpp
@@ -0,0 +1,116 @@
+﻿#include "GameCamera.hpp"
+
+CameraArea::CameraArea(Vec2 pos, Vec2 initPos, double width, double height, Vec2 velocity, double scale, bool isScroll)
+	: pos{ pos }
+	, initPos{ initPos }
+	, velocity{ velocity }
+	, width{ width }
+	, height{ height }
+	, scale{ scale }
+	, isScroll{ isScroll }
+	, area{ RectF{ pos, width, height } }
+{
+	cameraTargetPos = initPos;
+}
+
+void CameraArea::setPos(Vec2 pos)
+{
+	this->pos = pos;
+	area.setPos(pos);
+}
+
+void CameraArea::setWidth(double width)
+{
+	this->width = width;
+	area.setSize(width, this->height);
+}
+
+void CameraArea::setHeight(double height)
+{
+	this->height = height;
+	area.setSize(this->width, height);
+}
+
+void CameraArea::init()
+{
+	cameraTargetPos = initPos;
+}
+
+void CameraArea::update(const Player& player)
+{
+	if (isScroll)
+	{
+		if (player.isAlive)
+		{
+			cameraTargetPos += velocity * Scene::DeltaTime();
+		}
+	}
+	else
+	{
+		// プレイヤーの位置に基づいてカメラのターゲット位置を更新
+		cameraTargetPos = player.pos + Vec2{ player.velocity.x / 3, 0 };
+		cameraTargetPos.x = Clamp(cameraTargetPos.x, pos.x + Scene::CenterF().x * scale, pos.x + width - Scene::CenterF().x * scale);
+		cameraTargetPos.y = Clamp(cameraTargetPos.y, pos.y + Scene::CenterF().y * scale, pos.y + height - Scene::CenterF().y * scale);
+	}
+}
+
+void CameraArea::drawArea() const
+{
+	area.draw(Palette::Plum.withA(25)).drawFrame(1, 0, Palette::Fuchsia);
+	Circle{ initPos, 2 }.draw(Palette::Orange);
+}
+
+GameCamera::GameCamera()
+	: camera{ Camera2D{ Vec2{ 0, 0 }, 1.0, CameraControl::None_ } }
+	, activeArea{ std::make_shared<CameraArea>(Vec2{ 0, 0 }, Scene::CenterF()) }
+{
+	areas.clear();
+}
+
+void GameCamera::addArea(std::shared_ptr<CameraArea> area)
+{
+	areas << area;
+}
+
+bool GameCamera::isPlayerOutOfScreen(const Player& player)
+{
+	// プレイヤーが画面外にいるかどうかのチェック処理を実装する
+	return false;
+}
+
+void GameCamera::restart()
+{
+	activeArea->init();
+}
+
+void GameCamera::update(const Player& player)
+{
+	// プレイヤーの位置に基づいてアクティブな領域を更新
+	for (auto& area : areas)
+	{
+		if (player.pos.intersects(area->area))
+		{
+			activeArea = area;
+		}
+	}
+
+	activeArea->update(player);
+
+	// カメラの位置とスケールを更新
+	camera.setTargetCenter(activeArea->cameraTargetPos);
+	camera.setTargetScale(activeArea->scale);
+	camera.update();
+}
+
+void GameCamera::drawArea() const
+{
+	for (auto& area : areas)
+	{
+		area->drawArea();
+	}
+}
+
+Transformer2D GameCamera::createTransformer() const
+{
+	return camera.createTransformer();
+}
---
+++ Kocotum/GameCamera.hpp
@@ -0,0 +1,95 @@
+﻿#pragma once
+#include <Siv3D.hpp>
+#include "Player.hpp"
+
+/**
+ * @brief カメラの表示領域を表すクラス
+ */
+struct CameraArea
+{
+	/// @brief カメラの表示領域
+	RectF area;
+	/// @brief カメラ領域の位置
+	Vec2 pos;
+	/// @brief カメラを写す初期位置
+	Vec2 initPos;
+	/// @brief カメラの目標位置
+	Vec2 cameraTargetPos;
+	/// @brief カメラスクロール速度
+	Vec2 velocity;
+	/// @brief カメラ領域の幅と高さ
+	double width, height;
+	/// @brief カメラの倍率
+	double scale;
+	/// @brief スクロールするかどうか
+	bool isScroll;
+
+	/**
+	 * @brief CameraAreaのコンストラクタ
+	 * @param pos 領域の位置
+	 * @param initPos 初期位置
+	 * @param width 領域の幅
+	 * @param height 領域の高さ
+	 * @param velocity スクロール速度
+	 * @param scale カメラのスケール
+	 * @param isScroll スクロールするかどうか
+	 */
+	CameraArea(Vec2 pos, Vec2 initPos, double width = 1280, double height = 960, Vec2 velocity = Vec2(0, 0), double scale = 1.0, bool isScroll = false);
+
+	void setPos(Vec2 pos);
+	void setWidth(double width);
+	void setHeight(double height);
+	void init();
+	void update(const Player& player);
+	void drawArea() const;
+};
+
+/**
+ * @brief ゲームカメラを管理するクラス
+ */
+struct GameCamera
+{
+	Camera2D camera; ///< Siv3Dのカメラオブジェクト
+	Array<std::shared_ptr<CameraArea>> areas; ///< カメラ領域のリスト
+	std::shared_ptr<CameraArea> activeArea; ///< 現在アクティブなカメラ領域
+
+	/**
+	 * @brief GameCameraのコンストラクタ
+	 */
+	GameCamera();
+
+	/**
+	 * @brief カメラ領域を追加する
+	 * @param area 追加するカメラ領域
+	 */
+	void addArea(std::shared_ptr<CameraArea> area);
+
+	/**
+	 * @brief プレイヤーが画面外にいるかチェックする
+	 * @param player プレイヤーオブジェクト
+	 * @return プレイヤーが画面外にいる場合true
+	 */
+	bool isPlayerOutOfScreen(const Player& player);
+
+	/**
+	 * @brief カメラをリスタートする
+	 */
+	void restart();
+
+	/**
+	 * @brief カメラの状態を更新する
+	 * @param player プレイヤーオブジェクト
+	 */
+	void update(const Player& player);
+
+	/**
+	 * @brief カメラ領域を描画する
+	 */
+	void drawArea() const;
+
+	/**
+	 * @brief カメラのTransformer2Dオブジェクトを作成する
+	 * @return Transformer2Dオブジェクト
+	 */
+	Transformer2D createTransformer() const;
+};
