d21fc061260ab98bbe4a838ea7a10999ecb11e48
Tanorenn <kuronosu102938@gmail.com>
2024-07-06 22:57:14 +0900

    RadioButtonsを追加

---
+++ Kocotum/RadioButtons.cpp
@@ -0,0 +1,63 @@
+﻿#include "RadioButtons.hpp"
+
+RadioButtons::RadioButtons(Vec2 pos, Array<String> labels, size_t& index)
+	: index{ index }
+{
+	double width = 0;
+	for (int i = 0; i < labels.size(); i++)
+	{
+		// 各ラジオボタンを作成し、配列に追加
+		buttons << RadioButton(pos + Vec2{ width, 0 }, labels[i]);
+		width += buttons[i].getWidth();
+	}
+
+	if (not buttons.isEmpty())
+	{
+		// 最初のボタンを選択状態にする
+		buttons[0].setSelected(true);
+		index = 0;
+	}
+}
+
+void RadioButtons::setEnable(bool enable)
+{
+	// 全てのボタンの有効/無効状態を設定
+	for (auto& button : buttons)
+	{
+		button.setEnable(enable);
+	}
+}
+
+void RadioButtons::update()
+{
+	for (int i = 0; i < buttons.size(); i++)
+	{
+		if (buttons[i].isEnable())
+		{
+			// ボタンがクリックされた場合、そのインデックスを選択状態にする
+			if (buttons[i].getButton().leftClicked())
+			{
+				index = i;
+			}
+
+			// 選択状態を更新
+			if (index == i)
+			{
+				buttons[i].setSelected(true);
+			}
+			else
+			{
+				buttons[i].setSelected(false);
+			}
+		}
+	}
+}
+
+void RadioButtons::draw() const
+{
+	// 全てのボタンを描画
+	for (auto& button : buttons)
+	{
+		button.draw();
+	}
+}
---
+++ Kocotum/RadioButtons.hpp
@@ -0,0 +1,31 @@
+﻿#pragma once
+#include <Siv3D.hpp>
+#include "RadioButton.hpp"
+
+/// @brief 複数のラジオボタンをグループ化して管理するクラス
+class RadioButtons
+{
+private:
+	/// @brief ラジオボタンの配列
+	Array<RadioButton> buttons;
+
+	/// @brief 選択されているボタンのインデックスへの参照
+	size_t& index;
+
+public:
+	/// @brief RadioButtonsのコンストラクタ
+	/// @param pos グループの開始位置
+	/// @param labels 各ラジオボタンのラベル
+	/// @param index 選択されているボタンのインデックスへの参照
+	RadioButtons(Vec2 pos, Array<String> labels, size_t& index);
+
+	/// @brief 全てのラジオボタンの有効/無効を設定する
+	/// @param enable 有効にする場合はtrue、無効にする場合はfalse
+	void setEnable(bool enable);
+
+	/// @brief ラジオボタングループの状態を更新する
+	void update();
+
+	/// @brief ラジオボタングループを描画する
+	void draw() const;
+};
