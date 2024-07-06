69424b4fe066f1763d52568dcabf6aee082ea137
Tanorenn <kuronosu102938@gmail.com>
2024-07-06 22:03:12 +0900

    GravityLineを追加

---
+++ Kocotum/GravityLine.cpp
@@ -0,0 +1,143 @@
+﻿#include "GravityLine.hpp"
+
+GravityLineHorizontal::GravityLineHorizontal(Vec2 pos, Effect& effect, Player& player, double length)
+	: Object{ pos, effect, player, E_ObjectType::GravityLineHorizontal, U"重力線:横" }
+	, length{ length }
+	, mouseOverBody{ RectF{ pos, CHIP_SIZE } }
+	, body{ RectF{ pos + Vec2{ 0, CHIP_SIZE.y / 2 }, length, 5 } }
+	, isTouched{ false }
+{ }
+
+void GravityLineHorizontal::restart()
+{
+	isTouched = false;
+}
+
+bool GravityLineHorizontal::intersectsPlayer()
+{
+	return body.intersects(player.body);
+}
+
+bool GravityLineHorizontal::mouseOver()
+{
+	return mouseOverBody.mouseOver();
+}
+
+void GravityLineHorizontal::setPos(Vec2 pos)
+{
+	this->pos = pos;
+	mouseOverBody.setPos(pos);
+	body.setPos(pos + Vec2{ 0, CHIP_SIZE.y / 2.0 });
+}
+
+void GravityLineHorizontal::handleCollisionX()
+{
+	if (!isTouched)
+	{
+		// プレイヤーの重力を反転させる
+		player.velocity.y = 5;
+		player.gravityDirection *= -1;
+		player.isGravityReverse = !player.isGravityReverse;
+		isTouched = true;
+	}
+}
+
+void GravityLineHorizontal::handleCollisionY()
+{
+	if (!isTouched)
+	{
+		// プレイヤーの重力を反転させる
+		player.velocity.y = 5;
+		player.gravityDirection *= -1;
+		player.isGravityReverse = !player.isGravityReverse;
+		isTouched = true;
+	}
+}
+
+void GravityLineHorizontal::update()
+{
+	body.setSize(length, 5);
+	mouseOverBody.setSize(length, CHIP_SIZE.y);
+
+	if (!intersectsPlayer())
+	{
+		isTouched = false;
+	}
+}
+
+void GravityLineHorizontal::draw() const
+{
+	double alpha = (isTouched ? 0.5 : 1);
+	body.draw(ColorF(1, alpha));
+}
+
+GravityLineVertical::GravityLineVertical(Vec2 pos, Effect& effect, Player& player, double length)
+	: Object{ pos, effect, player, E_ObjectType::GravityLineVertical, U"重力線:縦" }
+	, length{ length }
+	, mouseOverBody{ RectF{ pos, CHIP_SIZE } }
+	, body{ RectF{ pos + Vec2{ CHIP_SIZE.x / 2, 0 }, 5, length } }
+	, isTouched{ false }
+{ }
+
+void GravityLineVertical::restart()
+{
+	isTouched = false;
+}
+
+bool GravityLineVertical::intersectsPlayer()
+{
+	return body.intersects(player.body);
+}
+
+bool GravityLineVertical::mouseOver()
+{
+	return mouseOverBody.mouseOver();
+}
+
+void GravityLineVertical::setPos(Vec2 pos)
+{
+	this->pos = pos;
+	mouseOverBody.setPos(pos);
+	body.setPos(pos + Vec2{ CHIP_SIZE.x / 2.0, 0 });
+}
+
+void GravityLineVertical::handleCollisionX()
+{
+	if (!isTouched)
+	{
+		// プレイヤーの重力を反転させる
+		player.velocity.y = 5;
+		player.gravityDirection *= -1;
+		player.isGravityReverse = !player.isGravityReverse;
+		isTouched = true;
+	}
+}
+
+void GravityLineVertical::handleCollisionY()
+{
+	if (!isTouched)
+	{
+		// プレイヤーの重力を反転させる
+		player.velocity.y = 5;
+		player.gravityDirection *= -1;
+		player.isGravityReverse = !player.isGravityReverse;
+		isTouched = true;
+	}
+}
+
+void GravityLineVertical::update()
+{
+	body.setSize(5, length);
+	mouseOverBody.setSize(CHIP_SIZE.x, length);
+
+	if (!intersectsPlayer())
+	{
+		isTouched = false;
+	}
+}
+
+void GravityLineVertical::draw() const
+{
+	double alpha = (isTouched ? 0.5 : 1);
+	body.draw(ColorF(1, alpha));
+}
---
+++ Kocotum/GravityLine.hpp
@@ -0,0 +1,63 @@
+﻿#pragma once
+#include <Siv3D.hpp>
+#include "Object.hpp"
+#include "Player.hpp"
+#include "Effect.hpp"
+
+/**
+ * @brief 横方向の重力線オブジェクトを表すクラス
+ */
+struct GravityLineHorizontal : public Object
+{
+	bool isTouched; ///< プレイヤーが触れたかどうか
+	double length; ///< 重力線の長さ
+	RectF mouseOverBody; ///< マウスオーバー判定用の矩形
+	RectF body; ///< 重力線の当たり判定用の矩形
+
+	/**
+	 * @brief GravityLineHorizontalクラスのコンストラクタ
+	 * @param pos 重力線の位置
+	 * @param effect エフェクト参照
+	 * @param player プレイヤー参照
+	 * @param length 重力線の長さ（デフォルトはCHIP_SIZE.x）
+	 */
+	GravityLineHorizontal(Vec2 pos, Effect& effect, Player& player, double length = CHIP_SIZE.x);
+
+	void restart() override;
+	bool intersectsPlayer() override;
+	bool mouseOver() override;
+	void setPos(Vec2 pos) override;
+	void handleCollisionX() override;
+	void handleCollisionY() override;
+	void update() override;
+	void draw() const override;
+};
+
+/**
+ * @brief 縦方向の重力線オブジェクトを表すクラス
+ */
+struct GravityLineVertical : public Object
+{
+	bool isTouched; ///< プレイヤーが触れたかどうか
+	double length; ///< 重力線の長さ
+	RectF mouseOverBody; ///< マウスオーバー判定用の矩形
+	RectF body; ///< 重力線の当たり判定用の矩形
+
+	/**
+	 * @brief GravityLineVerticalクラスのコンストラクタ
+	 * @param pos 重力線の位置
+	 * @param effect エフェクト参照
+	 * @param player プレイヤー参照
+	 * @param length 重力線の長さ（デフォルトはCHIP_SIZE.y）
+	 */
+	GravityLineVertical(Vec2 pos, Effect& effect, Player& player, double length = CHIP_SIZE.y);
+
+	void restart() override;
+	bool intersectsPlayer() override;
+	bool mouseOver() override;
+	void setPos(Vec2 pos) override;
+	void handleCollisionX() override;
+	void handleCollisionY() override;
+	void update() override;
+	void draw() const override;
+};
