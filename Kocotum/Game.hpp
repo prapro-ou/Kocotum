39931ba47d7a5191fe935f1990d320094a4f4900
Tanorenn <kuronosu102938@gmail.com>
2024-07-08 03:11:47 +0900

    Gameシーンを追加

---
+++ Kocotum/Game.hpp
@@ -0,0 +1,38 @@
+﻿# pragma once
+# include <Siv3D.hpp>
+# include "Effect.hpp"
+# include "Object.hpp"
+# include "Player.hpp"
+# include "Objects.hpp"
+# include "World.hpp"
+# include "GameData.hpp"
+
+class Game : public App::Scene
+{
+public:
+
+	Game(const InitData& init)
+		:IScene{ init }
+	{
+		getData().world.restart();
+	}
+
+
+	void update() override
+	{
+		getData().world.update();
+
+		if (KeyEnter.down())
+		{
+			changeScene(U"Edit");
+		}
+	}
+
+	void draw() const override
+	{
+		{
+			const Transformer2D cameraTransformer = getData().world.camera.createTransformer();
+			getData().world.draw();
+		}
+	}
+};
