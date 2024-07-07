96086fdfe74652a4d085d9e481eb3654dae15cba
Tanorenn <kuronosu102938@gmail.com>
2024-07-08 05:52:09 +0900

    TextBoxを追加

---
+++ Kocotum/TextBox.cpp
@@ -0,0 +1,60 @@
+﻿# include "TextBox.hpp"
+
+
+TextBox::TextBox(Vec2 pos, double width, uint16 fontSize, String value)
+{
+	this->pos = pos;
+	this->width = Max(width, 50.0);
+	this->fontSize = fontSize;
+	this->font = Font(fontSize, Typeface::Regular);
+	this->height = font.fontSize() + 20;
+	this->region = RectF(pos, width, height);
+	this->value = value;
+	this->editingText = U"";
+	this->isActive = false;
+}
+
+
+String TextBox::getText()
+{
+	return value;
+}
+
+
+void TextBox::setText(String text)
+{
+	this->value = text;
+}
+
+void TextBox::update()
+{
+	// 領域がクリックされていたらアクティブ
+	if (MouseL.down())
+	{
+		isActive = region.mouseOver();
+	}
+
+	if (isActive)
+	{
+		// キーボードからテキストを入力
+		TextInput::UpdateText(value);
+
+		// 未変換の文字入力を取得
+		editingText = TextInput::GetEditingText();
+	}
+}
+
+void TextBox::draw() const
+{
+	region.draw().drawFrame(2, (isActive ? Palette::Dodgerblue : Palette::Dimgray));
+	String tmp = U"";
+	if (isActive)
+	{
+		tmp = value + U'|' + editingText;
+	}
+	else
+	{
+		tmp = value + editingText;
+	}
+	font(tmp).draw(region, Palette::Black);
+}
---
+++ Kocotum/TextBox.hpp
@@ -0,0 +1,54 @@
+﻿# pragma once
+# include <Siv3D.hpp>
+
+
+/// @brief テキストを入力することができるUIを提供します。
+class TextBox
+{
+private:
+	/// @brief テキストボックスの座標
+	Vec2 pos;
+	/// @brief テキストボックスの幅
+	double width;
+	/// @brief テキストボックスの高さ
+	double height;
+	/// @brief フォントの大きさ
+	uint16 fontSize;
+	/// @brief フォント
+	Font font;
+	/// @brief テキストボックスの領域
+	RectF region;
+	/// @brief テキストボックスに入力されている値
+	String value;
+	/// @brief 編集中のテキスト
+	String editingText;
+	/// @brief テキストボックスが選択されているかどうか
+	bool isActive;
+
+public:
+	/// @brief デフォルトコンストラクターです。何もしません。
+	SIV3D_NODISCARD_CXX20
+		TextBox() = default;
+
+	/// @brief テキストボックスを作成します。
+	/// @param pos 座標
+	/// @param width 幅
+	/// @param fontSize フォントのサイズ
+	/// @param maxDigit 最大文字数
+	/// @param value 初期値
+	TextBox(Vec2 pos, double width = 350, uint16 fontSize = 50, String value = U"0");
+
+	/// @brief テキストボックスの値を取得します。
+	/// @return 入力されているテキスト
+	String getText();
+
+	/// @brief テキストボックスの値を設定します。
+	/// @param text 設定するテキスト
+	void setText(String text);
+
+	/// @brief テキストボックスを更新します。
+	void update();
+
+	/// @brief テキストボックスを描画します。
+	void draw() const;
+};
