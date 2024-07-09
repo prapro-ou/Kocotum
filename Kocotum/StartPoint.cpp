ae6a5cb5de951e45a25167765e1ee434786c1489
Tanorenn <kuronosu102938@gmail.com>
2024-07-10 03:42:09 +0900

    ファイル分け

---
+++ Kocotum/StartPoint.cpp
@@ -0,0 +1,50 @@
+﻿#include "StartPoint.hpp"
+
+StartPoint::StartPoint(Vec2 pos, World& world)
+	:Object{ pos, world, E_ObjectType::StartPoint, U"スタート位置" }
+	, body{ RectF{ pos, CHIP_SIZE } }
+{ }
+
+
+void StartPoint::restart()
+{
+
+}
+
+
+bool StartPoint::intersectsPlayer()
+{
+	//return false;
+	return body.intersects(world.player.body);
+}
+
+bool StartPoint::mouseOver()
+{
+	return body.mouseOver();
+}
+
+void StartPoint::setPos(Vec2 pos)
+{
+	this->pos = pos;
+	body.setPos(pos);
+}
+
+void StartPoint::handleCollisionX()
+{
+
+}
+
+void StartPoint::handleCollisionY()
+{
+
+}
+
+void StartPoint::update()
+{
+
+}
+
+void StartPoint::draw() const
+{
+	body.draw(ColorF(0.25, 0.5, 0.5, 0.25));
+}
