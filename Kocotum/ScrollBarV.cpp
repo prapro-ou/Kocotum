49b9b92d998e40d489882507512a25769f3ac4c2
Tanorenn <kuronosu102938@gmail.com>
2024-07-10 01:47:38 +0900

    縦方向のスクロールバーを追加したヨ

---
+++ Kocotum/ScrollBarV.cpp
@@ -0,0 +1,98 @@
+﻿#include "ScrollBarV.hpp"
+
+double logN(double x, double n)
+{
+	return logf(x) / logf(n);
+}
+
+ScrollBarV::ScrollBarV(Vec2 pos, double width, double height, double min, double max)
+{
+	this->pos = pos;
+	this->min = min;
+	this->max = max;
+	this->Area = RectF{ pos, width, height };
+	this->Box = RectF{ pos, width, height };
+	this->grabbed = false;
+	this->max = max;
+	this->min = min;
+	this->now = 0;
+}
+
+bool ScrollBarV::isGrabbed()
+{
+	return this->grabbed;
+}
+
+void ScrollBarV::setMax(double max)
+{
+	this->max = max;
+}
+
+void ScrollBarV::setMin(double min)
+{
+	this->min = min;
+}
+
+void ScrollBarV::setNow(double now)
+{
+	this->now = now;
+}
+
+void ScrollBarV::setInfo(double min, double max, double now)
+{
+	setMin(min);
+	setMax(max);
+	setNow(now);
+}
+
+double ScrollBarV::getNow()
+{
+	return this->now;
+}
+
+void ScrollBarV::update()
+{
+	// 最大値と最小値の差
+	double range = max - min;
+	// スクロールバーのつまみの長さ
+	Box.setSize(Box.w, Max(Area.h - logN(range, Area.h * 5) * (Area.h - 30), 50.0));
+	// スクロールバーの可動域
+	double length = Area.h - Box.h;
+	// 1ピクセル当たりの増加量
+	double step = range / length;
+
+
+	//バーが掴まれていたら
+	if (grabbed)
+	{
+		//クリックされたままなら値更新
+		if (MouseL.pressed())
+		{
+			now = min + step * (Cursor::PosF().y - pos.y - Box.h / 2.0);
+			now = Clamp(now, min, max);
+		}
+		//離されたらフラグ更新
+		else
+		{
+			grabbed = false;
+		}
+	}
+	//バーが離されていたら
+	else
+	{
+		//マウスが乗っていてクリックしたらフラグ更新
+		if (Area.mouseOver() && MouseL.down()) grabbed = true;
+	}
+
+	// ボックスの座標更新
+	Box.setPos(Box.pos.x, Area.pos.y + (now - min) / range * length);
+}
+
+/// @brief スクロールバーを描画します。
+void ScrollBarV::draw()
+{
+	//Area.rounded(8).draw(Palette::Ghostwhite).drawFrame(1, ColorF(0.5));
+	//if ((isMouseOver() && !MouseL.pressed()) || grabbed) Area.rounded(8).draw(ColorF(0.65, 0.65, 0.85, 0.15));
+	Box.rounded(8).draw(ColorF(0.35, 0.75));
+	if (Box.mouseOver() || grabbed) Box.rounded(8).draw(ColorF(1, 0.15));
+}
---
+++ Kocotum/ScrollBarV.hpp
@@ -0,0 +1,75 @@
+﻿#pragma once
+#include <Siv3D.hpp>
+
+/// @brief 底がnのlogを返します。
+/// @param x 値
+/// @param n 底
+/// @return 値
+double logN(double x, double n);
+
+
+/// @brief 縦向きのスクロールバーを提供します。
+class ScrollBarV
+{
+private:
+	/// @brief スクロールバーの座標
+	Vec2 pos;
+	/// @brief スクロールバーの領域
+	RectF Area;
+	/// @brief スクロールバーのつまみ
+	RectF Box;
+	/// @brief スクロールバーが掴まれているかのフラグ
+	bool grabbed;
+	/// @brief 最大値
+	double max;
+	/// @brief 最小値
+	double min;
+	/// @brief 現在の値
+	double now;
+
+public:
+	SIV3D_NODISCARD_CXX20
+		ScrollBarV() = default;
+
+	/// @brief 縦向きのスクロールバーを作成します。
+	/// @param pos スクロールバーの座標
+	/// @param width スクロールバーの幅
+	/// @param height スクロールバーの高さ
+	/// @param min 最小値
+	/// @param max 最大値
+	/// @param offset オフセット
+	ScrollBarV(Vec2 pos, double width, double height, double min, double max);
+
+	/// @brief スクロールバーが掴まれているかを返します。
+	/// @return スクロールバーが掴まれている場合 true, それ以外の場合は false
+	bool isGrabbed();
+
+	/// @brief 最大値を設定します。
+	/// @param max 最大値
+	void setMax(double max);
+
+	/// @brief 最小値を設定します。
+	/// @param min 最小値
+	void setMin(double min);
+
+	/// @brief 現在の値を設定します。
+	/// @param now 現在値
+	void setNow(double now);
+
+	/// @brief 最小値, 最大値, 現在の値を設定します。
+	/// @param min 最小値
+	/// @param max 最大値
+	/// @param now 現在の値
+	void setInfo(double min, double max, double now);
+
+	/// @brief 現在の値を取得します。
+	/// @return 現在の値
+	double getNow();
+
+	/// @brief スクロールバーを更新します。
+	void update();
+
+	/// @brief スクロールバーを描画します。
+	void draw();
+
+};
