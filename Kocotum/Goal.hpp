d896f526c67594048b5e36b3384cd8908ab67e16
Tanorenn <kuronosu102938@gmail.com>
2024-10-28 15:00:27 +0900

    Revert "ゴールシーン"
    This reverts commit 2bf54f727b3ddffe5ab562300f15f97b920a543d.

--- Kocotum/Goal.hpp
+++ Kocotum/Goal.hpp
@@ -9,10 +9,6 @@
 
 class Goal : public App::Scene
 {
-private:
-	const Font font{ 80, Typeface::Bold };
-	const Font sfont{ 50, Typeface::Bold };
-
 public:
 
 	Goal(const InitData& init)
@@ -26,21 +22,14 @@ public:
 	{
 		if (KeyEnter.down())
 		{
-			getData().deathCount = 0;
-			getData().sw.reset();
-			getData().world.warpFileName = U"data/map/title.csv";
-			getData().world.warp();
+			changeScene(U"Edit");
 			getData().world.restart();
 			getData().world.update();
-			changeScene(U"Play");
 		}
 	}
 
 	void draw() const override
 	{
-		font(U"クリア！").drawAt(Scene::CenterF());
-		sfont(U"プレイ時間:{:.2f}"_fmt(getData().sw.sF())).drawAt(Scene::CenterF().movedBy(0, 150));
-		sfont(U"リトライ回数:{}"_fmt(getData().deathCount)).drawAt(Scene::CenterF().movedBy(0, 250));
-		sfont(U"Enterで初めからプレイ").drawAt(Scene::CenterF().movedBy(0, 350));
+
 	}
 };
